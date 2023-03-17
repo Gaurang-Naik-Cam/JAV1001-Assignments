8b8093ddb9ca1c101cec8639fe5a0e3811ef13cb
Gaurang Naik <A00250808@mycambrian.ca>
2023-03-17 00:03:47 -0400

    Added delete and clear button functionality
    Added button functionality and Changed the colour scheme

--- Lab 6/app/src/main/java/com/example/todolist/MainActivity.kt
+++ Lab 6/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
-
+import androidx.core.util.set
+import androidx.core.view.get
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,15 +19,38 @@ class MainActivity : AppCompatActivity() {
         var itemlist = arrayListOf<String>()
         var adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,itemlist)
         val btnAdd :Button = findViewById(R.id.btnAdd)
+        val btnDelete :Button = findViewById(R.id.btnDelete)
+        val btnClear :Button = findViewById(R.id.btnClear)
         val editTextView: EditText = findViewById(R.id.txtInput)
         val listView :ListView = findViewById(R.id.ListView)
 
         btnAdd.setOnClickListener {
-            itemlist.add(editTextView.text.toString())
-            listView.adapter = adapter
-            adapter.notifyDataSetChanged()
-            editTextView.text.clear()
+            val state = listView.onSaveInstanceState()
+            if(editTextView.text.isNotEmpty()) {
+                itemlist.add(editTextView.text.toString())
+                listView.adapter = adapter
+                adapter.notifyDataSetChanged()
+                editTextView.text.clear()
+            }
+            listView.onRestoreInstanceState(state)
         }
 
+        btnClear.setOnClickListener {
+            itemlist.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        btnDelete.setOnClickListener{
+            val positions = listView.checkedItemPositions;
+            var items = listView.count - 1;
+            while(items >= 0){
+                if(positions.get(items)) {
+                        adapter.remove(itemlist.get(items))
+                }
+                items--
+             }
+            positions.clear()
+            adapter.notifyDataSetChanged()
+        }
     }
 }
