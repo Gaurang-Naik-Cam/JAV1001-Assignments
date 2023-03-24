52f468d80aa78ae0c94c9bab1b810161340dcfa3
Gaurang Naik <A00250808@mycambrian.ca>
2023-03-24 15:42:56 -0400

    Initial Design
    First Commit

---
+++ MyContactsApp/app/src/main/java/com/example/mycontactsapp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.mycontactsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import androidx.core.util.set
+import androidx.core.view.get
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var itemlist = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
+        val btnAdd: Button = findViewById(R.id.btnSave)
+        //val btnDelete: Button = findViewById(R.id.btnDelete)
+       // val btnClear: Button = findViewById(R.id.btnClear)
+        val editTextView: EditText = findViewById(R.id.txtInput)
+        val listView: ListView = findViewById(R.id.ListView)
+
+        btnAdd.setOnClickListener {
+            val state = listView.onSaveInstanceState()
+            if (editTextView.text.isNotEmpty()) {
+                itemlist.add(editTextView.text.toString())
+                listView.adapter = adapter
+                adapter.notifyDataSetChanged()
+                editTextView.text.clear()
+            }
+            listView.onRestoreInstanceState(state)
+        }
+
+/* btnClear.setOnClickListener {
+     itemlist.clear()
+     adapter.notifyDataSetChanged()
+ }*/
+
+/* btnDelete.setOnClickListener {
+     val positions = listView.checkedItemPositions;
+     var items = listView.count - 1;
+     while (items >= 0) {
+         if (positions.get(items)) {
+             adapter.remove(itemlist.get(items))
+         }
+         items--
+     }
+     positions.clear()
+     adapter.notifyDataSetChanged()
+
+ }*/
+}
+}
